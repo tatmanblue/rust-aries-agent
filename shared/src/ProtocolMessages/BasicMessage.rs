7d1cd970f85efeb4d8aceb3994d137cfeeb856b4
tatmanblue <Matt.Raffel@gmail.com>
2020-08-13 16:22:18 -0600

    implementing constructors
    Signed-off-by: tatmanblue <Matt.Raffel@gmail.com>

--- shared/src/ProtocolMessages/BasicMessage.rs
+++ shared/src/ProtocolMessages/BasicMessage.rs
@@ -1,5 +1,4 @@
-
-
+use std::io::Error;
 
 #[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -19,4 +18,23 @@ pub struct BasicMessage {
 #[serde(rename_all = "camelCase")]
 pub struct L10N {
     pub locale: String,
+}
+
+impl BasicMessage {
+    pub fn new(id: String) -> BasicMessage {
+        BasicMessage {
+            id,
+            type_field: "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/basicmessage/1.0/message".to_string(),
+            l10n: L10N {
+                locale: "".to_string()
+            },
+            sent_time: "".to_string(),
+            content: "".to_string(),
+        }
+    }
+    
+    pub fn from_json(json: &str) -> Result<BasicMessage, Error> {
+        let message: BasicMessage = serde_json::from_str(json)?;
+        Ok(message)
+    }
 }
